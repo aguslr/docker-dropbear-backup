3dad40fb1586f91102d54839ec3e966561f68c9e
Agus Lopez <contact@aguslr.com>
2023-03-11 17:40:28 +0100

    Fix warning about existing directories

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,8 @@
 # Create backup user
 if [ "${BACKUP_USER}" ] && ! grep -s "^${BACKUP_USER}" /etc/passwd; then
 	adduser --shell /bin/sh --uid "${BACKUP_UID:-11000}" --disabled-password "${BACKUP_USER}" && \
-	(cd "/home/${BACKUP_USER}" && mkdir borgbackup rsync sftp) && \
-	chown "${BACKUP_USER}":"${BACKUP_USER}" "/home/${BACKUP_USER}/"*
+	(cd "/home/${BACKUP_USER}" && mkdir -p borgbackup rsync sftp) && \
+	chown -R "${BACKUP_USER}":"${BACKUP_USER}" "/home/${BACKUP_USER}/"*
 fi
 
 # Start AirSane
