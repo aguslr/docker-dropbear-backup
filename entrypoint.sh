7889d3e076c7dd614ba802986f7513e6396a6a01
Agus Lopez <contact@aguslr.com>
2023-03-11 17:36:39 +0100

    Fix undefined brace expansion in POSIX shell

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 # Create backup user
 if [ "${BACKUP_USER}" ] && ! grep -s "^${BACKUP_USER}" /etc/passwd; then
 	adduser --shell /bin/sh --uid "${BACKUP_UID:-11000}" --disabled-password "${BACKUP_USER}" && \
-	mkdir -p "/home/${BACKUP_USER}/"{borgbackup,rsync,sftp} && \
+	(cd "/home/${BACKUP_USER}" && mkdir borgbackup rsync sftp) && \
 	chown "${BACKUP_USER}":"${BACKUP_USER}" "/home/${BACKUP_USER}/"*
 fi
 
